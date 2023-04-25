ebe7d34c56877d6b20503e57bb5c9d13b2ffbc98
vinayagaratnamthirulogachandran <misterthiru610@gmail.com>
2023-04-25 10:32:53 +0530

    my first crud

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+const UserRoute = require('./routes/User.js')
+
+
+
+const app = express();
+
+app.use('/user',UserRoute)
+
+app.use(bodyParser.urlencoded({extended : true}))
+
+app.use(bodyParser.json())
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Databse Connected Successfully!!");    
+}).catch(err => {
+    console.log('Could not connect to the database', err);
+    process.exit();
+});
+
+app.get('/', (req, res) => {
+    res.json({"message": "Hello Crud Node Express"});
+});
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
